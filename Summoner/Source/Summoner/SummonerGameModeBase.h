2e076506b4061ea90bd7e361e4cac6b8496509cb
KINGGUN5151 <ma_khanzadeh@yahoo.com>
2020-08-25 01:44:10 -0700

    Start

---
+++ Summoner/Source/Summoner/SummonerGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "SummonerGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SUMMONER_API ASummonerGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
